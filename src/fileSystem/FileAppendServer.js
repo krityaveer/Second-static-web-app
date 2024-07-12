788e92e1c55b5cccccbe710df6d02a30305543a8
krityaveer <krityaveersingh@gmail.com>
2024-07-13 00:10:55 +0530

    4thformdata

--- src/fileSystem/FileAppendServer.js
+++ src/fileSystem/FileAppendServer.js
@@ -16,7 +16,7 @@ const FileAppendServer = () => {
     event.preventDefault();
 
     //fetch('http://localhost:5000/append', {
-    fetch('api\file-appender', {
+    fetch('api/file-appender/serverText.js', {
       method: 'POST',
       headers: {
         'Content-Type': 'application/json',
