cefde986d46c824e670649946fd06ecb8b182b52
sanoo singh <sanookumar17@gmail.com>
2024-07-20 15:58:50 -0500

    push updates

--- api/append_data/index.js
+++ api/append_data/index.js
@@ -1,21 +1,25 @@
  module.exports = async function (context, req) {
   
-    context.log('JavaScript HTTP trigger function processed a request.');
+    try {
+        // Parse JSON data from the request body
+        const jsonData = req.body;
 
-    // const name = (req.query.name || (req.body && req.body.name));
-    // const responseMessage = name
-    //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
-    //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
-    const { name, email } = req.body;
-    const newContent = `${name},  ${email}`;
-    // fs.appendFile(filePath, newContent, (err) => {
-    //     if (err) {
-    //       return res.status(500).json({ error: 'Failed to append to file' });
-    //     }
-    //     res.status(200).json({ message: 'Data appended successfully' });
-    //   });
-    context.res = {
-        // status: 200, /* Defaults to 200 */
-        body: name
-    };
+        // Process the JSON data as needed
+        console.log('Received JSON data:', jsonData);
+
+        // Send a response
+        context.res = {
+           //  headers: {
+           //      'Content-Type': 'application/json'
+           //    },
+              body:  JSON.stringify({jsonData})
+
+        };
+    } catch (error) {
+        // Handle errors
+        context.res = {
+            status: 500,
+            body: 'Error processing data: ' + error.message
+        };
+    }
 }
