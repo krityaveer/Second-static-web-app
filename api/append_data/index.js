8cff4c028c8dfc82a13b0a67ca33c1f71b3e9035
krityaveer <krityaveersingh@gmail.com>
2024-07-21 00:04:56 +0530

    static api

---
+++ api/append_data/index.js
@@ -0,0 +1,21 @@
+module.exports = async function (context, req) {
+  // const filePath = path.join("../file-appender/", 'append_info.txt');
+    context.log('JavaScript HTTP trigger function processed a request.');
+
+    // const name = (req.query.name || (req.body && req.body.name));
+    // const responseMessage = name
+    //     ? "Hello, " + name + ". This HTTP triggered function executed successfully."
+    //     : "This HTTP triggered function executed successfully. Pass a name in the query string or in the request body for a personalized response.";
+    const { name, email } = req.body;
+    const newContent = `${name},  ${email}`;
+    // fs.appendFile(filePath, newContent, (err) => {
+    //     if (err) {
+    //       return res.status(500).json({ error: 'Failed to append to file' });
+    //     }
+    //     res.status(200).json({ message: 'Data appended successfully' });
+    //   });
+    // context.res = {
+    //     // status: 200, /* Defaults to 200 */
+    //     body: responseMessage
+    // };
+}
